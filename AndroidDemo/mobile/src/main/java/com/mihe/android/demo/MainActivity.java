f39bd148a62820715772a5e25d66e823fa8c6cab
astroboy <astroboy.apple@gmail.com>
2016-11-04 15:23:44 +0800

    修复编译问题

--- AndroidDemo/mobile/src/main/java/com/mihe/android/demo/MainActivity.java
+++ AndroidDemo/mobile/src/main/java/com/mihe/android/demo/MainActivity.java
@@ -30,7 +30,6 @@ public class MainActivity extends AppCompatActivity {
 
     // Example of a call to a native method
     TextView tv = (TextView) findViewById(R.id.sample_text);
-    tv.setText(stringFromJNI());
     }
 
     @Override
@@ -55,14 +54,4 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
-
-    // Used to load the 'native-lib' library on application startup.
-    static {
-        System.loadLibrary("native-lib");
-    }
 }
